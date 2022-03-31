1d2610528bf69b3027416a5df38651c5c5a592dc
polemonLX <diogo.ventura6@hotmail.com>
2022-03-31 15:21:05 +0100

    First commit

---
+++ src/main/java/pt/viki/brosmod/BrosMod.java
@@ -0,0 +1,29 @@
+package pt.viki.brosmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = BrosMod.MODID, name = BrosMod.NAME, version = BrosMod.VERSION)
+public class BrosMod {
+
+    public static final String MODID = "brosmod";
+
+    public static final String NAME = "Bros Mod";
+
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+}
